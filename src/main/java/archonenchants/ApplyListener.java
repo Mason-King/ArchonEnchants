e80def5134fce985200d19cd3740e98682686539
Mason-King <73730655+Mason-King@users.noreply.github.com>
2021-04-28 09:58:25 -0500

    Added some checks

--- src/main/java/archonenchants/ApplyListener.java
+++ src/main/java/archonenchants/ApplyListener.java
@@ -1,8 +1,10 @@
 package archonenchants;
 
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -19,15 +21,41 @@ public class ApplyListener implements Listener {
 
     @EventHandler
     public void onClick(InventoryClickEvent e) {
+        Player p = (Player) e.getWhoClicked();
         ItemStack book = e.getCursor();
         ItemStack tooEnchant = e.getCurrentItem();
+        if(p.getGameMode().equals(GameMode.CREATIVE)) return;
+        if(!book.getType().equals(Material.matchMaterial(main.getConfig().getString("enchantmentBookMaterial")))) return;
         List<Enchantment> enchants = main.getCustomenchants();
         for(Enchantment ench : enchants) {
             for(Map.Entry entry : book.getEnchantments().entrySet()) {
-                if(ench.equals(entry.getKey()))              {
-                    enchantItem(tooEnchant, ench, (Integer) entry.getValue());
-                    e.setCancelled(true);
-                    book.setType(Material.AIR);
+                if(ench.equals(entry.getKey())) {
+                        if(tooEnchant.containsEnchantment(ench)) {
+                            if(tooEnchant.getEnchantmentLevel(ench) > (Integer) entry.getValue()) {
+                                System.out.println("higher");
+                                return;
+                            } else if(tooEnchant.getEnchantmentLevel(ench) < (Integer) entry.getValue()) {
+                                System.out.println("lower");
+                                tooEnchant.removeEnchantment(ench);
+                                enchantItem(tooEnchant, ench, (Integer) entry.getValue());
+                                e.setCancelled(true);
+                                p.setItemOnCursor(new ItemStack(Material.AIR));
+                                book.setType(Material.AIR);
+                            } else {
+                                System.out.println("equal");
+                                tooEnchant.removeEnchantment(ench);
+                                enchantItem(tooEnchant, ench, (Integer) entry.getValue() + 1);
+                                e.setCancelled(true);
+                                p.setItemOnCursor(new ItemStack(Material.AIR));
+                                book.setType(Material.AIR);
+                            }
+                        } else {
+                            enchantItem(tooEnchant, ench, (Integer) entry.getValue());
+                            e.setCancelled(true);
+                            p.setItemOnCursor(new ItemStack(Material.AIR));
+                            book.setType(Material.AIR);
+                        }
+                        return;
                 }
             }
         }
